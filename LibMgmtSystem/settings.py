1aa428be1fd5adb297ce2e8f38130613150d4f61
dipeshadk77 <78727798+dipeshadk77@users.noreply.github.com>
2022-04-15 23:11:04 +0545

    Update settings.py

--- LibMgmtSystem/settings.py
+++ LibMgmtSystem/settings.py
@@ -161,6 +161,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'library.sagarmatha@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sagarmatha1234'
+EMAIL_HOST_PASSWORD = '****************'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = ''
